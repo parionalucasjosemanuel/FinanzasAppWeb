bab9bfbc40afed383441f51d304822a4fa9528c2
parionalucasjosemanuel <105582093+parionalucasjosemanuel@users.noreply.github.com>
2025-11-29 00:31:27 -0500

    Delete src directory

--- src/models/loan.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Loan', {
-    amount: DataTypes.DECIMAL(12,2),
-    term_months: DataTypes.INTEGER,
-    annual_rate: DataTypes.DECIMAL(6,4),
-    start_date: DataTypes.DATEONLY,
-    bono_techo: { type: DataTypes.BOOLEAN, defaultValue: false }
-  });
-};
--- src/routes/auth.js
+++
@@ -1,5 +0,0 @@
-const r = require('express').Router();
-const c = require('../controllers/auth');
-r.post('/register', c.register);
-r.post('/login', c.login);
-module.exports = r;
